a18f8ff832fcba916fa534cb45a8fec1a448ff05
So <wolfiaspikky@gmail.com>
2023-02-08 11:24:44 +0100

    show list ingredients in table

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
+    #[Route('/ingredient', name: 'ingredient', methods: ['GET'])]
     public function index(IngredienRepository $repository,PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(
